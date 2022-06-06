512c93da7e0ed567c075617a48b21e903388aa6d
WallAshing <alexandre.vauthier@edu.devinci.fr>
2022-06-06 17:07:21 +0200

    bug fixing and did a bit of responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,26 @@ export class AppComponent {
   ngOnInit(){
 
   }
+
+  displayMenu(){
+    const menu = document.getElementById("menu")
+    const menuButton = document.getElementById("menu-button")
+    if (menu && menuButton && menu.parentElement) {
+      if (menu.style.display != "grid"){
+        menu.style.display = "grid"
+        menuButton.style.margin = "auto"
+        menu.parentElement.style.right = "0"
+        menu.parentElement.style.backgroundColor = "rgba(0, 0, 0, 0.95)"
+        menu.parentElement.style.width = "100%"
+      } else {
+        menu.style.display = "none"
+        menuButton.style.margin = "2vh auto"
+        menu.parentElement.style.right = "4vw"
+        menu.parentElement.style.background = "none"
+        menu.parentElement.style.width = "100px"
+
+      }
+    }
+  }
+
 }
--- src/app/cv/about-me/about-me.component.ts
+++ src/app/cv/about-me/about-me.component.ts
@@ -12,4 +12,19 @@ export class AboutMeComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  displayParagraph(): void{
+    const textBox: HTMLElement | null = document.getElementById("about-me-box")
+    const button: HTMLElement | null = document.getElementById("about-me-button")
+    if (textBox && button) {
+      if (textBox.style.display != "none"){
+        textBox.style.display = "none"
+        button.style.position = "initial"
+        button.style.paddingTop = "10px"
+      } else {
+        textBox.style.display = "block"
+        button.style.position = "absolute"
+        button.style.paddingTop = "0"
+      }
+    }
+  }
 }
